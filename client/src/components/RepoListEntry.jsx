7dc2652be20e2e0ec79b72ca1af13109a54866bf
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-24 16:45:12 -0700

    Add user profile link in list, and within database

--- client/src/components/RepoListEntry.jsx
+++ client/src/components/RepoListEntry.jsx
@@ -4,6 +4,7 @@ const RepoListEntry = (props) => (
   <div className="repo-list-entry">
     <ul className="repo">
       <li><b>Owner:</b> {props.repo.owner}</li>
+      <li><b>Owner Profile:</b> <a href={props.repo.ownerURL}>{props.repo.ownerURL}</a></li>
       <li><b>Repo Name:</b> {props.repo.repo_name}</li>
       <li><b>Repo URL:</b> <a href={props.repo.repo_url}>{props.repo.repo_url}</a></li>
       <li><b>Forks:</b> {props.repo.forks}</li>
